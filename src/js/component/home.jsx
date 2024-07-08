c8c418dd25d8a564dd0bbe762a3a485bdb9e2df4
Kevin Padilla <kevpadilla01@gmail.com>
2024-07-08 21:01:00 +0000

    proyecto corregido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -23,10 +23,13 @@ const Home = () => {
 	}
 
 	return (
-		<div className="d-flex flex-column justify-content-center w-100 h-100vh mx-auto bg-dark p-5 gap-2">
-			<button onClick={click} id="red" className={"red p-5 border border-5 rounded rounded-pill bg-danger "+ red} style={{width: '50px'}}></button>
-			<button onClick={click} id="yellow" className={"yellow bg-warning p-5 border border-5 rounded rounded-pill " + yellow} style={{width: '50px'}}></button>
-			<button onClick={click} id="green" className={"green bg-success p-5 border border-5 rounded rounded-pill "+ green} style={{width: '50px'}}></button>
+		<div className="d-flex flex-column align-items-center w-100 vh-100 bg-secondary">
+			<div className="bg-dark h-25" style={{width: '30px'}}></div>
+			<div className="d-flex flex-column bg-dark p-4 gap-2 rounded">
+				<button onClick={click} id="red" className={"red p-5 border border-5 rounded rounded-pill bg-danger "+ red} style={{width: '50px'}}></button>
+				<button onClick={click} id="yellow" className={"yellow bg-warning p-5 border border-5 rounded rounded-pill " + yellow} style={{width: '50px'}}></button>
+				<button onClick={click} id="green" className={"green bg-success p-5 border border-5 rounded rounded-pill "+ green} style={{width: '50px'}}></button>
+			</div>
 		</div>
 	);
 };
